011326c0f448599ffc8521f2eff0aa57d82ec1f7
jareckikck <jaroslawgrabarczyk01@gmail.com>
2019-11-11 21:39:36 +0100

    add ACF_OPTION_SLUG

--- inc/custom-fucntions.php
+++ inc/custom-fucntions.php
@@ -1,1 +1,25 @@
-<?php 
+<?php
+
+if (function_exists('pll_current_language')) {
+  define('ACF_OPTION_SLUG', pll_current_language('slug'));
+
+	if (function_exists('acf_add_options_page')) {
+    $lang_array = pll_languages_list();
+    foreach ($lang_array as $lang) {
+      acf_add_options_sub_page([
+        'page_title' => "Opcje dla języka " . $lang,
+        'menu_title' => __("Opcje dla języka " . $lang, 'projectic'),
+        'menu_slug' => "acf-options-${lang}",
+        'post_id' => $lang,
+        'parent_slug' => $parent['menu_slug'],
+      ]);
+    }
+	}
+	
+} else {
+	define('ACF_OPTION_SLUG', 'options');
+	
+  if (function_exists('acf_add_options_page')) {
+    acf_add_options_page();
+  }
+}
