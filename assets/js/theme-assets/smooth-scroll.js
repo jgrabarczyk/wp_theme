7ecee244658b07d26db93c60a30fc912b05f1104
jareckikck <jaroslawgrabarczyk01@gmail.com>
2019-12-04 21:35:41 +0100

    extract SmoothScroll class

--- assets/js/theme-assets/smooth-scroll.js
+++ assets/js/theme-assets/smooth-scroll.js
@@ -1,33 +1,51 @@
-let menuOffset = jQuery('.wp-header-menu').height() + 25; // menu selector + custom additional space
-
-jQuery(document).ready(function () {
-    setTimeout(function () {
-        if (location.hash) {
-            window.scrollTo(0, 0);
-            target = location.hash.split('#');
-            scrollToTarget(jQuery('#' + target[1]));
-        }
-    }, 0.5);
-    scrollInsidePage();
+jQuery(document).ready(function() {
+  let smoothScroll = new SmoothScroll();
 });
+class SmoothScroll {
+  constructor() {
+    this.menuOffset = jQuery(".wp-header-menu").height() + 25;
+    setTimeout(this.onInit, 500);
+  }
+  onInit = () => {
+    let classThis = this;
 
-function scrollInsidePage() {
-    jQuery('[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function () {
-        if (isPageLink(this)) {
-            scrollToTarget(jQuery(this.hash), true);
-        }
-    });
-}
+    if (!location.hash) return;
+    window.scrollTo(0, 0);
+    classThis.scrollToTarget(jQuery(location.hash));
+  };
 
-function scrollToTarget(target, preventDefault = false) {
-    target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
-    if (target.length === 0 ) return; 
-    if (preventDefault) { event.preventDefault(); }
-    jQuery('html, body').animate({
-        scrollTop: target.offset().top - menuOffset
-    }, 1000);
-}
+  scrollInsidePage = () => {
+    let classThis = this;
+    jQuery('[href*="#"]')
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function() {
+        if (!classThis.isPageLink(this)) return;
+        classThis.scrollToTarget(jQuery(this.hash), true);
+      });
+	};
+	
+  isPageLink = that => {
+    return (
+      location.pathname.replace(/^\//, "") ==
+        that.pathname.replace(/^\//, "") && location.hostname == that.hostname
+    );
+  };
 
-function isPageLink(that) {
-    return isPagelink = (location.pathname.replace(/^\//, '') == that.pathname.replace(/^\//, '') && location.hostname == that.hostname) ? true : false;     
+  scrollToTarget(target, preventDefault = false) {
+    let classThis = this;
+    target = target.length
+      ? target
+      : jQuery("[name=" + this.hash.slice(1) + "]");
+    if (target.length === 0) return;
+    if (preventDefault) {
+      event.preventDefault();
+    }
+    jQuery("html, body").animate(
+      {
+        scrollTop: target.offset().top - classThis.menuOffset
+      },
+      1000
+    );
+  }
 }
