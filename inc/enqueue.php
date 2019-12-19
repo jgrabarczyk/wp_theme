692b7cbbb38532409d2716d291596f589871c791
Jarosław Grabarczyk <jaroslawgrabarczyk01@gmail.com>
2019-12-19 16:24:09 +0100

    babel update
    We use babel now so, no need to enqueue all files. Just enqeue compiled one.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,28 +8,23 @@ function wp_theme_header_scripts() {
 
         wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
         wp_enqueue_script('modernizr'); // Enqueue it!        
-        
-        wp_register_script('default-scripts', get_template_directory_uri() . '/assets/js/theme-assets/default.js', array('jquery'), '1.0.0'); // Custom scripts
-        wp_enqueue_script('default-scripts'); // Enqueue it!
-
-        wp_register_script('searchformToggler', get_template_directory_uri() . '/assets/js/theme-assets/searchform-toggle.js', array('jquery'), '1.0.0'); // Custom scripts
-        wp_enqueue_script('searchformToggler'); // Enqueue it!
-
+                        
         wp_register_script('wp_theme-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
         wp_enqueue_script('wp_theme-scripts'); // Enqueue it!
-        
-        wp_register_script('smooth-scroll', get_template_directory_uri() . '/assets/js/theme-assets/smooth-scroll.js', array('jquery'), '1.0.0'); // Custom scripts
-        wp_enqueue_script('smooth-scroll'); // Enqueue it!
-        
-        wp_register_script('menu', get_template_directory_uri() . '/assets/js/theme-assets/menu.js', array('jquery'), '1.0.0'); // Custom scripts
-        wp_enqueue_script('menu'); // Enqueue it!
-
+                                
         wp_register_script('aos', get_template_directory_uri() . '/assets/js/lib/aos/aos.js', array('jquery'), '1.0.0'); // Custom scripts
         wp_enqueue_script('aos'); // Enqueue it!
         
-          // just uncomment to enable slick slider
+         // slick slider
         wp_register_script('slick', get_template_directory_uri() . '/assets/js/lib/slick/slick.min.js', array('jquery'), '1.8.0'); // Slick
         wp_enqueue_script('slick'); // Enqueue it!
+        
+        wp_register_script('slick-lightbox', get_template_directory_uri() . '/assets/js/lib/slick-lightbox/slick-lightbox.min.js', array('jquery'), '1.8.0'); 
+		wp_enqueue_script('slick-lightbox'); // Enqueue it!
+        
+        //all theme-assets scirpts compiled  with babel
+		wp_register_script('theme-assets', get_template_directory_uri() . '/assets/js/all.js', array('jquery'), '1.0.0'); 
+        wp_enqueue_script('theme-assets'); // Enqueue it!
     }
 }
 
